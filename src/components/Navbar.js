f6553d4cb5e709cbdfc9523e3299810e0eee6d20
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2025-05-24 07:27:39 +0400

    [svg] logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,15 @@ function Navbar() {
     return (
         <nav className="bg-white shadow">
             <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-                <Link to="/" className="text-xl font-bold text-gray-800">ProteinShaikh</Link>
+                <Link to="/" className="flex items-center space-x-2">
+                    {/* SVG Logo */}
+                    <img src={`${process.env.PUBLIC_URL}/logo.svg`} alt="Logo" className="w-8 h-8"/>
+
+                    {/* Stylish Brand Text */}
+                    <span className="text-2xl font-bold font-[Playfair_Display] text-black">ProteinShaikh</span>
+                </Link>
+
+
                 <div className="space-x-4">
                     <Link to="/about" className="text-gray-600 hover:text-gray-800">About</Link>
                     <Link to="/articles" className="text-gray-600 hover:text-gray-800">Articles</Link>
